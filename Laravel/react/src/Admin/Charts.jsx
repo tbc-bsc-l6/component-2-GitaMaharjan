5aa39a9003240d307952f54a2b3fb1c17382b378
Gita <gitamaharjan303@gmail.com>
2023-12-28 01:45:07 +0545

    feat: Implement Product component with CRUD operations
    Introduce Product component to display a list of products with basic CRUD operations.
    - Add CategoryController for managing category-related logic.
    - Update ProductController with changes related to category interactions.
    - Modify routes in api.php to include routes for the newly added CategoryController.
    - Implement search functionality to filter products based on name, category, and discount.
    - Add a memoized selector for filtered products in the Redux store.
    - Display product details in a table, including name, price, quantity, category, discount, image, date, and action links.
    - Implement functionality to delete a product using an API request.

---
+++ Laravel/react/src/Admin/Charts.jsx
@@ -0,0 +1,14 @@
+import {Link } from 'react-router-dom'
+
+const Charts = () => {
+  return (
+    <>
+
+   <Link to="/dashboard/products">View Products</Link>
+   <Link to="/dashboard/product-form"> Products</Link>
+
+    </>
+  )
+}
+
+export default Charts
