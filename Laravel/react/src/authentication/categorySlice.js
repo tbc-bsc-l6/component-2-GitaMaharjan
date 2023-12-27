48e152a7ee0d28240f665d66ea4203f2d966d4c8
Gita <gitamaharjan303@gmail.com>
2023-12-28 01:58:40 +0545

    Create productsSlice to manage the state of products in Redux. - Include actions for adding products, deleting products, updating products, and adding messages.
    Create categorySlice to manage the state of categories in Redux.
    - Include actions for adding categories, updating categories, and adding messages.

---
+++ Laravel/react/src/authentication/categorySlice.js
@@ -0,0 +1,40 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  category: [
+    {
+      name: "",
+      description: "",
+      date: "",
+    },
+  ],
+  message: "",
+};
+
+export const categorySlice = createSlice({
+  name: "category",
+  initialState,
+  reducers: {
+    add_category: (state, action) => {
+      let totalCategory = action.payload;
+      totalCategory.forEach((element) => {
+        element["nanoid"] = nanoid();
+      });
+      state.category = totalCategory;
+    },
+    update_category: (state, action) => {},
+
+    add_message: (state, action) => {
+      state.message = action.payload.mess;
+    },
+  },
+});
+
+export const {
+  add_category,
+  delete_category,
+  update_category,
+  add_message,
+} = categorySlice.actions;
+
+export default categorySlice.reducer;
