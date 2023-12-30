fae6dd1c51a76358f7ba211b0046b3c425b4089f
Gita <gitamaharjan303@gmail.com>
2023-12-30 13:25:47 +0545

    Refactor CategoryBody component to use a div layout with fixed-size cards

--- react/src/customer/collections/CategoryBody.jsx
+++ react/src/customer/collections/CategoryBody.jsx
@@ -1,3 +1,5 @@
+
+
 import * as React from 'react';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -5,133 +7,73 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { add_category } from '../../authentication/categorySlice';
+import { Link, useNavigate } from 'react-router-dom';
 
 const containerStyle = {
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   justifyContent: 'center',
-  minHeight: '100vh',
+  // minHeight: '100vh',
 };
 
 export default function CategoryBody() {
+  const dispatch = useDispatch();
+  const items = useSelector((state) => state.categoryReducer.category);
+
+  useEffect(() => {
+    axios
+      .get('http://127.0.0.1:8000/api/get-categories')
+      .then((response) => {
+        console.log(response);
+        dispatch(add_category(response.data.allCategories));
+      })
+      .catch((error) => {
+        console.error('Error fetching categories:', error);
+      });
+  }, [dispatch]);
+
   return (
     <div style={containerStyle}>
-      <ImageList sx={{ width: '80%', height: '80%', overflow: 'hidden', gap: '30px' }}>
-        <ImageListItem key="Subheader" cols={4} >
+      <ImageList >
+        <ImageListItem key="Subheader" cols={4}>
           <ListSubheader component="div">All Categories</ListSubheader>
         </ImageListItem>
-        {itemData.map((item) => (
-          <ImageListItem key={item.img} sx={{ width: '100%', height: '100%', gap: '30px' }}>
-            <img
-              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              src={`${item.img}?w=248&fit=crop&auto=format`}
-              alt={item.title}
-              loading="lazy"
-            />
-            <ImageListItemBar
-              title={item.title}
-              subtitle={item.author}
-              actionIcon={
-                <IconButton
-                  sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                  aria-label={`info about ${item.title}`}
-                >
-                  <InfoIcon />
-                </IconButton>
-              }
-            />
-          </ImageListItem>
-        ))}
+        {items ? (
+          items.map((category) => (
+            <ImageListItem
+              key={category.image}
+              sx={{ width: '300px', height: '200px', position: 'relative' }}
+            >
+              <img
+                src={`http://127.0.0.1:8000/storage/${category.image}`}
+                alt={category.name}
+                style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+              />
+              <ImageListItemBar
+                title={category.name}
+                subtitle={category.description}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                    aria-label={`info about ${category.description}`}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              />
+            </ImageListItem>
+          ))
+        ) : (
+          <div>No categories</div>
+        )}
       </ImageList>
     </div>
   );
 }
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
+
+
