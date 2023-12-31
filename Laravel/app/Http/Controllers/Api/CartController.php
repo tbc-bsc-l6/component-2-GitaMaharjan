e69fb131b1baeda20e9884d67c4a25711ec9a9d0
Gita <gitamaharjan303@gmail.com>
2023-12-31 22:10:39 +0545

    implemented isProductInCart method and enhance addProductToCart method
    This commit adds the isProductInCart method to the CartController, which checks if a product with the given user ID and product ID exists in the Cart table. This commit modifies the addProductToCart method in the CartController to improve functionality and readability. It checks if a product is already in the user's cart and updates the quantity if present. If not present, it creates a new entry in the Cart table with the specified quantity.

--- Laravel/app/Http/Controllers/Api/CartController.php
+++ Laravel/app/Http/Controllers/Api/CartController.php
@@ -38,5 +38,45 @@ class CartController extends Controller
             ]);
         }
     }
+
+    public function isProductInCart($userId, $productId){
+        $cartEntry = Cart::where(['user_id' => $userId, 'product_id' => $productId])->get();
+        return count($cartEntry) > 0;
+    }
+   
+
+
+    public function addProductToCart(Request $request){
+        $productId= $request->pro_id;
+        $quantity = $request->quantity;
+        $customerId = $request->cust_id;
+        $add = $request->add;
+        if(count(Cart::where(['product_id'=> $productId, 'user_id'=> $customerId])->get()->toArray()) > 0){
+            Cart::where(['product_id'=> $productId, 'user_id' => $customerId])->update(['quantity'=> $quantity]);     
+            $productResult = Product::where(['id'=> $productId])->get()->toArray();
+            $cartProduct = Cart::where(['product_id'=> $productId, 'user_id' => $customerId])->get()->toArray();
+            $category = Product::where('id', $productId)->get()[0]->get_category->get();
+                return response(['repeat'=> false, 'status'=> true, 'userid'=> $customerId, 'result'=> $productResult, 'cart_pr'=> $cartProduct, 'category'=> $category]);
+           
+        }
+        else if(count(Cart::where(['user_id'=> $customerId])->get()->toArray()) > 0){
+            Cart::create(['product_id'=>$productId, 'user_id'=>$customerId, 'quantity'=>$quantity]);
+            $productResult = Product::where(['id'=> $productId])->get()->toArray();
+            $cartProduct = Cart::where(['product_id'=> $productId, 'user_id' => $customerId, 'quantity'=> $quantity])->get()->toArray();
+            $category = Product::where('id', $productId)->get()[0]->get_category->get();
+
+            return response(['repeat'=> false, 'status'=>true, 'userid'=> $customerId ,'result'=>$productResult, 'cart_pr'=> $cartProduct, 'category'=> $category]);
+        }
+        else{
+            Cart::create(['product_id'=> $productId, 'user_id'=> $customerId, 'quantity'=> $quantity]);
+            $productResult = Product::where(['id'=> $productId])->get()->toArray();
+            $cartProduct = Cart::where(['product_id'=> $productId, 'user_id' => $customerId, 'quantity'=> $quantity])->get()->toArray();
+            $category = Product::where('id', $productId)->get()[0]->get_category->get();
+            return response(['repeat'=> false,'status'=>true, 'userid'=> $customerId, 'result'=> $productResult, 'cart_pr'=> $cartProduct, 'category'=> $category]);
+        }
+    }
+
+
+
    
 }
